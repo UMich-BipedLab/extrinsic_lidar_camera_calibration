e2d6abd61ffc108e20773c62e327091e4ef338eb
brucejk <bjhuang@umich.edu>
2019-10-09 16:49:59 -0400

    change size of markers

--- showLinedAprilTag.m
+++ showLinedAprilTag.m
@@ -5,7 +5,7 @@ function showLinedAprilTag(fig_handle, camera_target, display)
         display = 0;
     end
     hold(fig_handle, 'on');
-    scatter(fig_handle, camera_target.corners(1,:), camera_target.corners(2,:), 10, 'ro')
+    scatter(fig_handle, camera_target.corners(1,:), camera_target.corners(2,:), 20, 'yo')
     plot(fig_handle, camera_target.four_corners_line(1,:), camera_target.four_corners_line(2,:))
     
     if display
