1c3db67389a308e7bca9611f2fed081e51657895
Bruce JK Huang <bjhuang@umich.edu>
2019-10-16 13:31:32 -0400

    add display checking

--- showLinedLiDARTag.m
+++ showLinedLiDARTag.m
@@ -1,9 +1,4 @@
 function showLinedLiDARTag(image_hadle, bagfile, LiDARTag, display)
-    if strcmpi("display", display)
-        display = 1;
-    else
-        display = 0;
-    end
     hold(image_hadle, 'on');
 %             axis(app.LiDARTagFig, 'equal');
     scatter3(image_hadle, LiDARTag.pc_points(1,:), LiDARTag.pc_points(2,:), LiDARTag.pc_points(3,:), '.')
@@ -12,7 +7,8 @@ function showLinedLiDARTag(image_hadle, bagfile, LiDARTag, display)
     n_vec = [LiDARTag.centroid, LiDARTag.centroid + LiDARTag.normal_vector];
     plot3(image_hadle, n_vec(1, :), n_vec(2, :), n_vec(3, :));
     title(image_hadle, bagfile);
-    if display
+    
+    if checkDisplay(display)
         set(get(image_hadle,'parent'),'visible','on');% show the current axes
         axis 'equal'
     end
