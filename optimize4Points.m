99e8bef4e3066897b542a4987a667fac8226960c
brucejk <bjhuang@umich.edu>
2019-10-25 12:11:54 -0400

    prepare for random initialization for lidar target optimization

--- optimize4Points.m
+++ optimize4Points.m
@@ -1,5 +1,6 @@
-function [H_LC, P, cost] = optimize4Points(opt, X, Y, intrinsic, display)
-
+function [final_H, final_P, min_cost, final_result, results] = optimize4Points(opt, X, Y, intrinsic, display)
+        
+    % prepare for random initilization (not use for now)
     for i = 1:1
         theta_x = optimvar('theta_x', 1, 1,'LowerBound',-180,'UpperBound',180); % 1x1
         theta_y = optimvar('theta_y', 1, 1,'LowerBound',-180,'UpperBound',180); % 1x1
@@ -29,46 +30,61 @@ function [H_LC, P, cost] = optimize4Points(opt, X, Y, intrinsic, display)
         end
         
         options = optimoptions('fmincon', 'MaxIter',5e2, 'TolX', 1e-12, 'Display','off', 'FunctionTolerance', 1e-8, 'MaxFunctionEvaluations', 3e4);
-
         [sol, fval, ~, ~] = solve(prob, x0, 'Options', options);
         R_final = rotx(sol.theta_x) * roty(sol.theta_y) * rotz(sol.theta_z);
         H_LC = eye(4);
         H_LC(1:3, 1:3) = R_final;
         H_LC(1:3, 4) = sol.T';
-
-        if display
-            disp('H_LC: ')
-            disp('     R:')
-            disp(H_LC(1:3, 1:3))
-            disp('     T:')
-            disp(-inv(H_LC(1:3, 1:3))*H_LC(1:3, 4))
-            fval
-        end
         P = intrinsic * [eye(3) zeros(3,1)] * H_LC;
-        cost = fval;
-        results(i).cost = fval;
+        
+        results(i).total_cost = fval;
+        results(i).RMSE = sqrt(fval/size(Y,2));
         results(i).H = H_LC;
+        results(i).P = P;
+        if display
+            disp('-- H_LC: ')
+            disp('------- R:')
+            disp(H_LC(1:3, 1:3))
+            disp('------- T:')
+            disp(-inv(H_LC(1:3, 1:3))*H_LC(1:3, 4))
+            disp('-- cost:')
+            disp(results(i).total_cost)
+            disp('-- RMSE:')
+            disp(results(i).RMSE)
+        end 
     end
-    disp("std:")
-    disp(std([results(:).cost]))
-    [min_cost, k] = min([results(:).cost]);
-    disp("minimum cost:")
-    disp(min_cost)
-    disp('H_LC: ')
-    disp(' R:')
-    disp(results(k).H(1:3, 1:3))
-    disp(' RPY (XYZ):')
-    disp(rad2deg(rotm2eul(results(k).H(1:3, 1:3), "XYZ")))
-    disp(' T:')
-    disp(-inv(results(k).H(1:3, 1:3))*results(k).H(1:3, 4))
-    [max_cost, j] = max([results(:).cost]);
-    disp("max cost:")
-    disp(max_cost)
-    disp('H_LC: ')
-    disp(' R:')
-    disp(results(j).H(1:3, 1:3))
-    disp(' RPY (XYZ):')
-    disp(rad2deg(rotm2eul(results(j).H(1:3, 1:3), "XYZ")))
-    disp(' T:')
-    disp(-inv(results(j).H(1:3, 1:3))*results(j).H(1:3, 4))
+    
+    [min_cost, k] = min([results(:).total_cost]);
+    [max_cost, ~] = max([results(:).total_cost]);
+    [min_RMSE, ~] = min([results(:).RMSE]);
+    final_result.RMSE = min_RMSE;
+    final_result.std = std([results(:).total_cost]);
+    final_result.min_cost = min_cost;
+    final_result.max_cost = max_cost;
+    final_H = results(k).H;
+    final_P = results(k).P;
+    final_result.H = final_H;
+    final_result.P = final_P;
+    if display
+%         disp("std:")
+%         disp(final_result.std)
+%         disp("minimum cost:")
+%         disp(min_cost)
+%         disp('H_LC: ')
+%         disp(' R:')
+%         disp(results(k).H(1:3, 1:3))
+%         disp(' RPY (XYZ):')
+%         disp(rad2deg(rotm2eul(results(k).H(1:3, 1:3), "XYZ")))
+%         disp(' T:')
+%         disp(-inv(results(k).H(1:3, 1:3))*results(k).H(1:3, 4))
+%         disp("max cost:")
+%         disp(max_cost)
+%         disp('H_LC: ')
+%         disp(' R:')
+%         disp(results(j).H(1:3, 1:3))
+%         disp(' RPY (XYZ):')
+%         disp(rad2deg(rotm2eul(results(j).H(1:3, 1:3), "XYZ")))
+%         disp(' T:')
+%         disp(-inv(results(j).H(1:3, 1:3))*results(j).H(1:3, 4))
+    end
 end
