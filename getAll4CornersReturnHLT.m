46536acb1b8fd067a9f26ec8281794b6e20c194a
brucejk <bjhuang@umich.edu>
2019-10-25 12:10:45 -0400

    changed to structure input

--- getAll4CornersReturnHLT.m
+++ getAll4CornersReturnHLT.m
@@ -1,11 +1,39 @@
-function [bag_data, H_LT] = getAll4CornersReturnHLT(optimizeAllCorners, tag_num, opt, path, bag_data, num_scan, num_pose)
+function [bag_data, H_LT] = getAll4CornersReturnHLT(optimizeAllCorners, tag_num, opt, path, bag_data, opts)
 % scan_num: scan number of these corner
 % num_scan: how many scans accumulated to get the corners
 % function [LiDARTag, AprilTag, H_LT] = get4CornersReturnHLT(tag_num, opt, mat_file_path, pc_mat_file, bag_file, target_len, pc_iter, num_scan)
+    
+%     if show.lidar_target_optimization
+%         target_len = bag_data.lidar_target(tag_num).tag_size; 
+%         pc = loadPointCloud(path.mat_file_path, bag_data.lidar_target(tag_num).pc_file);
+%         for pc_iter = 1:5
+%             X = getPayload(pc, pc_iter, opts.num_scan);
+%             opt_temp_draw = opt.H_TL;
+%             [X_clean, scan_total_draw(pc_iter).clean_up] = cleanLiDARTargetWithOneDataSet(X, target_len, opt_temp_draw);
+% 
+%             cost
+%             opt_temp_draw = optimizeCost(opt_temp_draw, X_clean, target_len, scan_total_draw(pc_iter).clean_up.std/2);
+%             target_lidar = [0 -target_len/2 -target_len/2 1;
+%                             0 -target_len/2  target_len/2 1;
+%                             0  target_len/2  target_len/2 1;
+%                             0  target_len/2 -target_len/2 1]';
+% 
+%             corners = opt_temp_draw.H_opt \ target_lidar;
+%             corners = sortrows(corners', 3, 'descend')';
+%             centroid = mean(corners(1:3,:), 2);
+%             normals = cross(corners(1:3,1)-corners(1:3,2), corners(1:3,1)-corners(1:3,3));
+%             normals = normals/(norm(normals));
+%             if normals(1) > 0
+%                 normals = -normals;
+%             end
+%         end
+%     end
+
+
     if optimizeAllCorners
+        target_len = bag_data.lidar_target(tag_num).tag_size; 
         pc = loadPointCloud(path.mat_file_path, bag_data.lidar_target(tag_num).pc_file);
-        target_len = bag_data.lidar_target(tag_num).tag_size;
-        num_scan_total = size(pc, 1)-num_scan;
+        num_scan_total = size(pc, 1) - opts.num_scan;
         tic
         scan_total(num_scan_total) = struct();
         scan_total(num_scan_total).clean_up = [];
@@ -15,14 +43,14 @@ function [bag_data, H_LT] = getAll4CornersReturnHLT(optimizeAllCorners, tag_num,
         scan_total(num_scan_total).pc_points = [];
         scan_total(num_scan_total).centroid = [];
         scan_total(num_scan_total).normal_vector = [];
-        scan_total(num_scan_total).H = [];
+        scan_total(num_scan_total).H = []; %% H_LT
         parfor pc_iter = 1:num_scan_total
 %             if mod(pc_iter, 10) == 0 || pc_iter == num_scan_total || pc_iter == 1
 %                 fprintf("--- Working on scan: %i/%i\n", pc_iter, num_scan_total)
 %             end
             fprintf("--- Working on scan: %i/%i\n", pc_iter, num_scan_total)
-            X = getPayload(pc, pc_iter, num_scan);
-            opt_temp = opt;
+            X = getPayload(pc, pc_iter, opts.num_scan);
+            opt_temp = opt.H_TL;
             [X_clean, scan_total(pc_iter).clean_up] = cleanLiDARTargetWithOneDataSet(X, target_len, opt_temp);
 
             % cost
@@ -62,7 +90,7 @@ function [bag_data, H_LT] = getAll4CornersReturnHLT(optimizeAllCorners, tag_num,
                     similarity_table(pc_iter).scan(j).diff = norm(v);
                 end
             end
-            similarity_table(pc_iter).mins = sum(mink([similarity_table(pc_iter).scan(:).diff], num_pose));
+            similarity_table(pc_iter).mins = sum(mink([similarity_table(pc_iter).scan(:).diff], opts.num_lidar_target_pose));
         end
         save(path.save_dir + extractBetween(bag_data.bagfile,"",".bag") + '_all_scan_corners.mat');
     else
@@ -70,10 +98,10 @@ function [bag_data, H_LT] = getAll4CornersReturnHLT(optimizeAllCorners, tag_num,
     end
     
     [~, chosen_scan] = min([similarity_table(:).mins]);
-    [~, chosen_scans] = mink([similarity_table(chosen_scan).scan(:).diff], num_pose);
+    [~, chosen_scans] = mink([similarity_table(chosen_scan).scan(:).diff], opts.num_lidar_target_pose);
     
     H_LT = [];
-    for scan_num = 1:num_pose
+    for scan_num = 1:opts.num_lidar_target_pose
         current_scan = chosen_scans(scan_num);
         bag_data.lidar_target(tag_num).scan(scan_num).corners = scan_total(current_scan).corners;
         bag_data.lidar_target(tag_num).scan(scan_num).four_corners_line = point3DToLineForDrawing(scan_total(current_scan).corners);
