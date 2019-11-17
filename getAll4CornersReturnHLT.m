bca7eddac9448f9ca08b3ee5f1cd0ceb46a3f2e2
brucejk <bjhuang@umich.edu>
2019-11-16 22:46:16 -0500

    change input parameters and random selecting scans

--- getAll4CornersReturnHLT.m
+++ getAll4CornersReturnHLT.m
@@ -1,4 +1,4 @@
-function [bag_data, H_LT] = getAll4CornersReturnHLT(optimizeAllCorners, tag_num, opt, path, bag_data, opts)
+function [bag_data, H_LT] = getAll4CornersReturnHLT(tag_num, opt, path, bag_data, opts)
 % scan_num: scan number of these corner
 % num_scan: how many scans accumulated to get the corners
 % function [LiDARTag, AprilTag, H_LT] = get4CornersReturnHLT(tag_num, opt, mat_file_path, pc_mat_file, bag_file, target_len, pc_iter, num_scan)
@@ -30,11 +30,10 @@ function [bag_data, H_LT] = getAll4CornersReturnHLT(optimizeAllCorners, tag_num,
 %     end
 
 
-    if optimizeAllCorners
+    if opts.optimizeAllCorners
         target_len = bag_data.lidar_target(tag_num).tag_size; 
         pc = loadPointCloud(path.mat_file_path, bag_data.lidar_target(tag_num).pc_file);
         num_scan_total = size(pc, 1) - opts.num_scan;
-        tic
         scan_total(num_scan_total) = struct();
         scan_total(num_scan_total).clean_up = [];
         scan_total(num_scan_total).corners = [];
@@ -44,11 +43,16 @@ function [bag_data, H_LT] = getAll4CornersReturnHLT(optimizeAllCorners, tag_num,
         scan_total(num_scan_total).centroid = [];
         scan_total(num_scan_total).normal_vector = [];
         scan_total(num_scan_total).H = []; %% H_LT
+        
+        disp("------------------------------------------------------------")
+        disp("Optimizing using PARFOR, the numbers WILL NOT BE in ORDERED")
+        disp("------------------------------------------------------------")
+        tic
         parfor pc_iter = 1:num_scan_total
-%             if mod(pc_iter, 10) == 0 || pc_iter == num_scan_total || pc_iter == 1
-%                 fprintf("--- Working on scan: %i/%i\n", pc_iter, num_scan_total)
-%             end
-            fprintf("--- Working on scan: %i/%i\n", pc_iter, num_scan_total)
+            if mod(pc_iter, 10) == 0 || pc_iter == num_scan_total || pc_iter == 1
+                fprintf("--- Working on scan: %i/%i\n", pc_iter, num_scan_total)
+            end
+%             fprintf("--- Working on scan: %i/%i\n", pc_iter, num_scan_total)
             X = getPayload(pc, pc_iter, opts.num_scan);
             opt_temp = opt.H_TL;
             [X_clean, scan_total(pc_iter).clean_up] = cleanLiDARTargetWithOneDataSet(X, target_len, opt_temp);
@@ -92,15 +96,19 @@ function [bag_data, H_LT] = getAll4CornersReturnHLT(optimizeAllCorners, tag_num,
             end
             similarity_table(pc_iter).mins = sum(mink([similarity_table(pc_iter).scan(:).diff], opts.num_lidar_target_pose));
         end
-        save(path.save_dir + extractBetween(bag_data.bagfile,"",".bag") + '_all_scan_corners.mat');
+        save(path.save_dir + extractBetween(bag_data.bagfile,"",".bag") + '_' + tag_num + '_' + '_all_scan_corners.mat', 'similarity_table', 'scan_total');
     else
-        load(path.load_all_vertices + extractBetween(bag_data.bagfile,"",".bag") + "_all_scan_corners.mat");
+        load(path.load_all_vertices + extractBetween(bag_data.bagfile,"",".bag") + '_' + tag_num + '_' + '_all_scan_corners.mat');
     end
     
-    [~, chosen_scan] = min([similarity_table(:).mins]);
-    [~, chosen_scans] = mink([similarity_table(chosen_scan).scan(:).diff], opts.num_lidar_target_pose);
-    
+    if opts.use_top_consistent_vertices
+        [~, chosen_scan] = min([similarity_table(:).mins]);
+        [~, chosen_scans] = mink([similarity_table(chosen_scan).scan(:).diff], opts.num_lidar_target_pose);
+    else
+        chosen_scans = randperm(num_scan_total, opts.num_lidar_target_pose);
+    end
     H_LT = [];
+    
     for scan_num = 1:opts.num_lidar_target_pose
         current_scan = chosen_scans(scan_num);
         bag_data.lidar_target(tag_num).scan(scan_num).corners = scan_total(current_scan).corners;
